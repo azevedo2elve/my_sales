d9a0d7ea1d5069b79a2e1f4204331e5bf2afd75e
azevedo2elve <azevedogabriel00@gmail.com>
2025-10-16 18:27:58 -0300

    feat: adiciona rotas para gerenciamento de produtos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import productsRouter from '../../../modules/products/routes/ProductRoutes';
 
 const routes = Router();
 
 routes.get('/health', (req, res) => {
   return res.json({ message: "Hello Dev! I'm alive!" });
 });
+
+routes.use('/products', productsRouter);
 
 export default routes;
