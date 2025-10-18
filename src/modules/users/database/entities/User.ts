fb58f8627a77cd95c9ae2d0ef47f4c04445174f6
Gabriel Azevedo <azevedogabriel00@gmail.com>
2025-10-18 12:52:47 -0300

    feat: cria entidade User e repositório para gerenciamento de usuários

---
+++ src/modules/users/database/entities/User.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @Column({ type: 'text' })
+  email: string;
+
+  @Column({ type: 'text' })
+  password: string;
+
+  @Column({ type: 'text', nullable: true })
+  avatar: string | null;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+}
