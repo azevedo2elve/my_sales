4c7d39f1cdfd11f69c574f9347098e861af23a98
Gabriel Azevedo <azevedogabriel00@gmail.com>
2025-10-23 03:51:27 -0300

    fix: adiciona log para verificar a existência do arquivo de avatar do usuário

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,9 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 import productsRouter from '../../../modules/products/routes/product.routes';
 import usersRouter from '../../../modules/users/routes/user.routes';
 import sessionRouter from '../../../modules/users/routes/session.route';
 import avatarRouter from '../../../modules/users/routes/avatar.route';
+import uploadConfig from '@config/upload';
 
 const routes = Router();
 
@@ -14,5 +15,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
 routes.use('/avatar', avatarRouter);
+routes.use('/files', express.static(uploadConfig.directory));
 
 export default routes;
