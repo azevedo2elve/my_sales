bde392706af08e80153c2b434762c0355ebae9fc
Gabriel Azevedo <azevedogabriel00@gmail.com>
2025-10-23 18:29:39 -0300

    feat: adiciona entidade e repositório para gerenciamento de tokens de usuário

---
+++ src/shared/infra/typeorm/migrations/1761243988815-CreateUserToken.ts
@@ -0,0 +1,56 @@
+import { Table, type MigrationInterface, type QueryRunner } from 'typeorm';
+
+export class CreateUserToken1761243988815 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKUserToken',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          }, // 1 (user) : N (tokens)
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Implementar o rollback aqui
+  }
+}
