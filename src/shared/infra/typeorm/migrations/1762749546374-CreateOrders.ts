29ff46dd2329e31225b4264cff64f83c5b18d1f7
Gabriel Azevedo <azevedogabriel00@gmail.com>
2025-11-10 01:51:46 -0300

    feat: cria migração para adicionar coluna customer_id na tabela orders

---
+++ src/shared/infra/typeorm/migrations/1762749546374-CreateOrders.ts
@@ -0,0 +1,37 @@
+import { Table, type MigrationInterface, type QueryRunner } from 'typeorm';
+
+export class CreateOrders1762749546374 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('orders');
+    if (table) {
+      await queryRunner.dropTable('orders');
+    }
+  }
+}
